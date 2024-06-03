1573246449c419ed9b002f6bec6447bbecbbfbf2
Parikshit7009 <jangra.parikshit@outlook.com>
2024-06-03 10:21:16 +0530

    add countword program

--- file/count_words.py
+++ file/count_words.py
@@ -1,7 +1,9 @@
-F=open("ParagraphParagraphs are the group of sen","r")
-Read=F.read()
-word=Read.split()
-for i in word:
-    c=c+1
-print(c)
-F.close()
+def countword():
+    F=open("ParagraphParagraphs are the group of sen","r")
+    Read=F.read()
+    word=Read.split()
+    for i in word:
+        c=c+1
+        print(c)
+    F.close()
+countword()
