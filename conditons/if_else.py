33dc419e8a1bd0d2da1ceedb180644ddf1d0736c
Parikshit7009 <jangra.parikshit@outlook.com>
2024-06-09 13:25:13 +0530

    Folder Name Change

---
+++ conditons/if_else.py
@@ -0,0 +1,5 @@
+Age=int(input("enter the Age "))
+if Age >=18:
+    print("You can Vote")
+else:
+    print("You Can't Vote")
--- loops/for.py
+++ loops/for.py
@@ -2,4 +2,3 @@ for i in range(0,10,1):
     for j in range(i+1,1):
         print(i,end="")
     print()
-    
