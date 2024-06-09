51c2b12cfdd3042694a6093c8f9a8589ada152c6
Parikshit7009 <jangra.parikshit@outlook.com>
2024-06-09 15:06:17 +0530

    can select shape b/w triangle and square/rectangle

--- Loop And Condition/Triangle.py
+++ Loop And Condition/Triangle.py
@@ -1,17 +1,26 @@
-#Triange made using loop
+#Triange AND Square  made using loop
 F="NnNOnoNo"#use this string in line 15
 G="YyYESYesyes"#use this string in line 17
+d='rectangle square'
 while True:
+    o=input("Select your shape Triangle or (square/ rectangle) ")
     s=int(input("Enter a Number which should be greater than 4-:"))
     m=input("enter the design")
     if s<4:
         print("Error")
-        break #loop break 
+        break
     else:
-        for i in range(0,s,1):
-            for j in range(0,i+1,1):
-                print(m,end="")
-            print()
+        if o in d:
+            v=int(input("enter the value"))
+            for i in range(0,s,1):
+                for j in range(0,v,1):
+                    print(m,end="")
+                print()
+        else:
+            for i in range(0,s,1):
+                for j in range(0,i+1,1):
+                    print(m,end="")
+                print()
     n=input("want to continue or not Y OR N")
     if n in F:
         break
