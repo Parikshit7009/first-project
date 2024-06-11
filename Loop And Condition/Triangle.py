2b108cf67f5a739c76a35c19f59bdab590c7e1e1
Parikshit <jangra.parikshit@outlook.com>
2024-06-11 13:29:59 +0530

    no change

--- Loop And Condition/Triangle.py
+++ Loop And Condition/Triangle.py
@@ -1,16 +1,16 @@
 #Triange AND Square  made using loop
-F="NnNOnoNo"#use this string in line 15
-G="YyYESYesyes"#use this string in line 17
-d="rectangle square"
+F="NnNOnoNo"# this string is in use in line 15
+G="YyYESYesyes"#this string is in use in line 17
+D="rectangle square"#this string is in use in line 13
 while True:
     o=input("Select your shape Triangle or (square/ rectangle) ")
-    s=int(input("Enter a Number which should be greater than 4-:"))
     m=input("enter the design")
+    s=int(input("Enter a Number which should be greater than 4-:"))
     if s<4:
         print("Error")
         break
     else:
-        if o in d:
+        if o in D:
             v=int(input("enter the value"))
             for i in range(0,s,1):
                 for j in range(0,v,1):
