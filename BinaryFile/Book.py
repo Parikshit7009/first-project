159ae097d73fed062b8567f8281db6f633cc91d5
Parikshit7009 <jangra.parikshit@oulook.com>
2024-06-18 20:46:00 +0530

    return no book by the author

---
+++ BinaryFile/Book.py
@@ -0,0 +1,23 @@
+import pickle
+def createfile():
+    fobj=open("book.dat","wb")
+    bookno=int(input("enter the book number"))
+    book_name=input("enter the book name")
+    author=input("enter the author nanme")
+    price=int(input("enter the price of book"))
+    rec=[bookno,book_name,author,price]
+    pickle.dump(rec,fobj)
+    fobj.close()
+createfile()
+
+
+def countrec():
+    fobj=open("book.dat","rb")
+    num=0
+    while True:
+        rec=pickle.load(fobj)
+    if author==rec[2]:
+        num=mum+1
+    fobj.close()
+countrec()
+
