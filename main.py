9e8dc356dc996e3251a60f29c36456437b861d13
Parikshit7009 <jangra.parikshit@oulook.com>
2024-06-18 20:31:58 +0530

    Remove extra Append ststement

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-
-
 def list1():
     l=[]
     let=int(input("enter the range of list"))
     for i in range (0,let,1):
         a=int(input("enter the number"))
         l.append(a)
-        l.append(a)
         # This is some code. Not written by me . XD
     print(l)
-
     return l
-
-#call function
-list1()
-
+list1() #Function Call
