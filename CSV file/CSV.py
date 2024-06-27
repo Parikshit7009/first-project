f3c8f98242489c45cebd093996c467ea58f1b8e7
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-27 23:11:30 +0530

    First CSV program

---
+++ CSV file/CSV.py
@@ -0,0 +1,23 @@
+import csv
+M="NOnoNo"
+Y="yesYesYES"
+def create():
+    f=open("Student.csv","w",newline='')
+    obj=csv.DictWriter(f)
+    header=["Student Name","Roll_No","Class","Age"]
+    obj.writerow(header)
+    while True:
+        Std=input("enter the student name:-")
+        rollno=int(input("enter the roll no:-"))
+        Class=int(input("enter the class:-"))
+        Age=int(input("enter the age:-"))
+        rec=[Std,rollno,Class,Age]
+        obj.writerow(rec)
+        ch=input("Want to add more data or not Yes/NO:-")
+        if ch==M:
+            break
+        elif ch==Y:
+            continue
+        else:
+            print("unknown Character enter -ERROR 404-")
+            break
