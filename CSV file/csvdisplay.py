214e2c3ac2c3c28db21f0238492dbe69c7ef18cf
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 00:41:47 +0530

    display the data from csv file

---
+++ CSV file/csvdisplay.py
@@ -0,0 +1,13 @@
+import csv
+def display():
+    f=open("Student.csv","r")
+    name=input("enter the student name")
+    obj=csv.reader(f)
+    next(f)
+    for i in obj:
+        if i[0]==name:
+            print(i)
+        else:
+            print("No such data available by this name") 
+    f.close()
+display()
