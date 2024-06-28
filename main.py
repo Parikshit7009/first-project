10ab337c38cb6942d1d0b2ddc067049ef70148ee
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 01:23:44 +0530

    file name change

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-def list1():
-    l=[]
-    let=int(input("enter the range of list"))
-    for i in range (0,let,1):
-        a=int(input("enter the number"))
-        l.append(a)
-        # This is some code. Not written by me . XD
-    print(l)
-    return l
-list1() #Function Call
+from csvadd import create
+from csvdisplay import display
