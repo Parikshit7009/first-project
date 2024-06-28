ab0106b95e48218e19f4003cee2c3c308151393f
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 01:07:38 +0530

    name statement added to loop

--- CSV file/csvdisplay.py
+++ CSV file/csvdisplay.py
@@ -1,14 +1,19 @@
 import csv
 def display():
     f=open("Student.csv","r")
-    name=input("enter the student name")
+    
     obj=csv.reader(f)
     next(f)
-    for i in obj:
-        if i[0]==name:
-            print(i)
-            print(len(str(i))*"-")
-        else:
-            print("No such data available by this name") 
+    while True:
+        name=input("enter the student name")
+        for i in obj:
+            if i[0]==name:
+                print(i)
+                print(len(str(i))*"-")
+            else:
+                print("No such data available by this name") 
+        ch=input("want to read more data")
+        if ch =="n":
+            break
     f.close()
 display()
