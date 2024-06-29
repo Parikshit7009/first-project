324111a48ce0bf56cef2a7306ab9c5bebf8e0220
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 15:29:14 +0530

    header prg

---
+++ CSV_file/header.py
@@ -0,0 +1,7 @@
+import csv
+def Header():
+    f=open("Student1.csv","a",newline='')
+    obj=csv.writer(f)
+    n=input("want to add header if no inter ")
+    header=["Student Name","Roll_No","Class","Age"]
+    obj.writerow(header)
