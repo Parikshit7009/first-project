a48619e9fee34b4c1d0e7d66c6d4fb4192c05ce5
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 15:06:41 +0530

    remove else statement

--- CSV_file/csvdisplay.py
+++ CSV_file/csvdisplay.py
@@ -3,15 +3,14 @@ def display():
     f=open("Student1.csv","r")
     
     obj=csv.reader(f)
-    next(f)
+    
     while True:
+        next(f)
         name=input("enter the student name")
         for i in obj:
             if i[0]==name:
                 print(i)
                 print(len(str(i))*"-")
-            else:
-                print("No such data available by this name") 
         ch=input("want to read more data")
         if ch =="n":
             break
