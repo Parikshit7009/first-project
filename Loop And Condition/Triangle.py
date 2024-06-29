b40efdac131cbc2ee8a03e50366c53a6f9bd43bb
Parikshit <146872066+Parikshit7009@users.noreply.github.com>
2024-06-29 15:35:53 +0530

    my opinion

--- Loop And Condition/Triangle.py
+++ Loop And Condition/Triangle.py
@@ -1,8 +1,14 @@
+#Triange made using loop
+F="NnNOnoNo"#use this string in line 15
+G="YyYESYesyes"#use this string in line 1
 #Triange AND Square  made using loop
 F="NnNOnoNo"# this string is in use in line 15
 G="YyYESYesyes"#this string is in use in line 17
 D="rectangle square"#this string is in use in line 13
 while True:
+    s=int(input("Enter a Number which should be greater than 4-:"))
+    m=input("enter the design")
+
     o=input("Select your shape Triangle or (square/ rectangle) ")
     m=input("enter the design")
     s=int(input("Enter a Number which should be greater than 4-:"))
@@ -10,6 +16,10 @@ while True:
         print("Error(Value might be less than 4)")
         break
     else:
+        for i in range(0,s,1):
+            for j in range(0,i+1,1):
+                print(m,end="")
+            print()
         if o in D:
             v=int(input("enter the value"))
             for i in range(0,s,1):
