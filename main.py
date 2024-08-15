8439d0b269e7734b29f98435c6972179ad652316
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-15 20:28:16 +0530

    Work start

--- main.py
+++ main.py
@@ -2,16 +2,27 @@
 from CSV_file.csvadd import *
 from CSV_file.csvdisplay import *
 from CSV_file.header import *
-while True:
+from LoopAndCondition.Triangle import *
+FUN=int(input("Select one option from above="))
+if FUN==1:
     print("1. Add Header")
+    print("---------------")
     print("2. Add Data ")
+    print("---------------")
     print("3. Display the data")
+    print("---------------")
     ch=int(input("SELECT ONE OPTION FORM ABOVE OPTION "))
-    if ch == 1:
-        Header()
-    elif ch ==2:
-        Add_Data()
-    elif ch==3:
-        display()
-    else:
-        print("wrong input")
+    while True:
+        if ch == 1:
+           Header()
+        elif ch ==2:
+           Add_Data()
+        elif ch==3:
+           display()
+        else:
+           print("wrong input")
+           break
+elif FUN==2:
+   shape()
+elif FUN==3:
+   
