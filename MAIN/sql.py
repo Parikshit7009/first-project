7e32ca305e11ec088d797e91482a4684c007f06c
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-15 21:01:56 +0530

    NAME

--- MAIN/sql.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-conn=mysql.connector.connect(host='localhost',username='root',password='6618',database='school')
-my_cursor=conn.cursor()
-
-conn.commit()
-conn.close()
-print("connect")
