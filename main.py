4366b8009d4e59929b2f4f1f36a7ccc90e610797
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-15 19:20:09 +0530

    started

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #in progress
+from CSV_file.csvadd import *
+from CSV_file.csvdisplay import *
+from CSV_file.header import *
+while True:
+    print("1. Add Header")
+    print("2. Add Data ")
+    print("3. Display the data")
+    ch=int(input("SELECT ONE OPTION FORM ABOVE OPTION "))
+    if ch == 1:
+        Header()
+    elif ch ==2:
+        Add_Data()
+    elif ch==3:
+        display()
+    else:
+        print("wrong input")
