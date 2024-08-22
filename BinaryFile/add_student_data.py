96181309a17c0f329dbc08de5b597ecbcf4baf13
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-23 00:35:32 +0530

    display prg and for loop covert to wile loop

--- BinaryFile/add_student_data.py
+++ BinaryFile/add_student_data.py
@@ -1,13 +1,24 @@
-import pickle
-F=open("Student.dat","wb")
-L={}
-c=int(input("how many student data you want to enter"))
-for i in range(0,c,1):
-    Class=int(input("Enter the class-:"))
-    Name=input("enter the nane of student-: ")
-    roll_no=int(input("Enter the roll number"))
-    Section=input("Enter the section")
-    L[Name]=[Class,Section,roll_no]
-pickle.dump(L,F)
 
-F.close()
+def binary():
+   F=open("Student.dat","wb")
+   L={}
+   c=int(input("how many student data you want to enter"))
+   for i in range(0,c,1):
+       Class=int(input("Enter the class-:"))
+       Name=input("enter the nane of student-: ")
+       roll_no=int(input("Enter the roll number"))
+       Section=input("Enter the section")
+       L[Name]=[Class,Section,roll_no]
+       pickle.dump(L,F)
+   F.close()
+
+
+def bindisplay():
+    F=open("Student.dat","rb")
+    read=pickle.load(F)
+    N=input("enter the name ")
+    for i in read:
+        c=i.split()
+        if N in c:
+            print()
+    F.close()
