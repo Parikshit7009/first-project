c70cf31a19345cee9da397679768eb3fe6e19c49
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-23 00:41:53 +0530

    for loop covert to wile loop

--- BinaryFile/add_student_data.py
+++ BinaryFile/add_student_data.py
@@ -1,16 +1,16 @@
 
 def binary():
-   F=open("Student.dat","wb")
-   L={}
-   c=int(input("how many student data you want to enter"))
-   for i in range(0,c,1):
+    F=open("Student.dat","wb")
+    L={}
+    c=int(input("how many student data you want to enter"))
+    while True:
        Class=int(input("Enter the class-:"))
        Name=input("enter the nane of student-: ")
        roll_no=int(input("Enter the roll number"))
        Section=input("Enter the section")
        L[Name]=[Class,Section,roll_no]
        pickle.dump(L,F)
-   F.close()
+    F.close()
 
 
 def bindisplay():
