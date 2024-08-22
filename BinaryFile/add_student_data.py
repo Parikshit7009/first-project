39c6b815cfd88e3f4c090a78270f5970d62ac4d7
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-08-23 00:44:12 +0530

    choice to continue add and unwanted input statement remove

--- BinaryFile/add_student_data.py
+++ BinaryFile/add_student_data.py
@@ -2,7 +2,6 @@
 def binary():
     F=open("Student.dat","wb")
     L={}
-    c=int(input("how many student data you want to enter"))
     while True:
        Class=int(input("Enter the class-:"))
        Name=input("enter the nane of student-: ")
@@ -10,6 +9,14 @@ def binary():
        Section=input("Enter the section")
        L[Name]=[Class,Section,roll_no]
        pickle.dump(L,F)
+       ch=input("Want to add more data ENTER Y(YES)/N(No):-")
+       if ch=="N":
+            break
+       elif ch=="Y":
+            continue
+       else:
+            print("unknown Character enter -ERROR 404- ")
+            break
     F.close()
 
 
