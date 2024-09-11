a25f3a7ed5a24745f5f86469a7cd85d8aea50518
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-09-11 11:25:32 +0530

    file name change

--- file/Line_more_than_10_words.py
+++
@@ -1,9 +0,0 @@
-def long():
-    f=open("line.txt","r")
-    lines=f.readlines()
-    for i in lines:
-        line=i.split()
-        if len(line) <= 10:
-            print(i)
-    f.close()
-long()
--- file/count_words.py
+++
@@ -1,9 +0,0 @@
-def countword():
-    F=open("ParagraphParagraphs are the group of sen","r")
-    Read=F.read()
-    word=Read.split()
-    for i in word:
-        c=c+1
-        print(c)
-    F.close()
-countword()
