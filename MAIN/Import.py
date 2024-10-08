f42c0f197986a51d42f95ad07b496ec694f45a21
Parikshit7009 <146872066+Parikshit7009@users.noreply.github.com>
2024-10-08 14:56:09 +0530

    remove unwanted import statment

--- MAIN/Import.py
+++ MAIN/Import.py
@@ -3,10 +3,6 @@ from CSV_file.csvdisplay import *
 from CSV_file.header import *
 from LoopAndCondition.Triangle import *
 from MAIN.Print import *
-import pickle 
-import csv
 from BinaryFile.add_student_data import *
 from BinaryFile.Book import *
 from file1.count_words import*
-import os
-from HelloWorld.HelloWorld import *
